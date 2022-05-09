a03e900899c6017c2774291ddd91f8f96043263f
araks1397 <razieh.izanloo755@gmail.com>
2022-05-09 23:42:30 +0430

    feat:edit  and add code for checklogin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from "react";
+// import { useNavigate } from "react-router-dom";
+// import CheckLogin from "../context/checkLogin";
+// import { useDispatch } from "react-redux";
+// import { setUser } from "../store/UserSlice";
 import { Link } from 'react-router-dom'
 import WithUser from '../Layouts/WithUser'
+import axios from 'axios'
+import Card from "../Components/Card";
+import { valueContext } from '../Context/Contexts'
 
- function Home() {
+function Home() {
+  const [products, setProduct] = useState()
+  const { data, setData } = useContext(valueContext);
+
+  // const url = 'http://localhost:3002/orders';
+  // function getData() {
+  //   axios({
+  //     url: 'http://localhost:3002/orders',
+  //     method: 'get',
+  //     params: {
+  //       token: 'TOP-SECRET'
+  //     }
+  //   })
+  //     .then(function (response) {
+  //       setData(response.data)
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }
+
+  // useEffect(() => { getData() }, [])
+  // console.log(data != null ? data : "kkk")
   return (
     <div>
-      <p><Link to='./Category'> گروه لبنیات</Link></p>
+      <p><Link to='./Category'> دمنوش های گیاهی</Link></p>
+      {/* <p>{products[0]?.name}</p> */}
+      {/* {product.map((products, i) => {
+          return (
+           <Card products={products} key={i} />
+          )
+        })} */}
+
+      {/* <p>
+          {Object.keys(products).map((item, i) => (
+            <p key={i} >
+              {item}
+            </p>
+          ))
+          }
+        </p> */}
+
       <p><Link to='/detail'>عکس محصولات</Link></p>
     </div>
   )
-}export default WithUser(Home)
+} export default WithUser(Home)
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,104 +1,103 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { Formik, replace } from 'formik';
-import { useNavigate, useLocation } from 'react-router';
-import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import {  useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { login} from '../Redux/Reducer'
+import { login } from '../Redux/LoginSlice'
 import axios from 'axios'
 
-function Login (){ 
-  const [errorlogin,setErorr] = useState({
-    usernameErr:'',
-    passwordErr:''
+function Login() {
+  const [errorlogin, setErorr] = useState({
+    usernameErr: '',
+    passwordErr: ''
   })
-  const [admin, setAdmin] = useState()
-  const state = useSelector((state) => state.admin.isLogin)
+  const [dataAdmin, setAdmin] = useState()
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const location = useLocation()
-  const redirectPath = location.state?.from.pathname || '/paneladmin'
-  const url='http://localhost:3002/whoami';
- 
-  const getInfo =()=>{
+  const url = 'http://localhost:3002/whoami';
+
+  const getInfo = () => {
     axios.get(url)
-    .then(res => setAdmin(res.data))
+      .then(res => setAdmin(res.data))
       .catch(error => console.log(error))
   }
-  useEffect(() => {getInfo()}, [])
-  return( 
-  <div className='login'>
-    <h1>ورود به پنل ادمین</h1>
-    <Formik
-      initialValues={{ name: '' }}
-      validate={values => {
-        let errors = {};
-        if (!values.username) {
-          errors.username = 'Required';
-        }
-        if (!values.password) {
-          errors.password = 'Required';
-        }
-        else if (values.password.length < 3) {
-          errors.password = "password is short"
-        }
-        return errors;
-      }}
-      onSubmit={(values, actions) => {
-        if(values.username !==admin.username){
-          setErorr({usernameErr:"نام کاربری وارد شده اشتباه است"})
-        } 
-          if(values.password !==admin.password){
-          setErorr({passwordErr:"پسورد وارد شده اشتباه است"})
-
-        }
-          else if(values.username==admin.username && values.password==admin.password){
-            dispatch(login(true))
-            navigate('/paneladmin/orders' , {replace})
+  useEffect(() => { getInfo() }, [])
+  
+  return (
+    <div className='login'>
+      <h1>ورود به پنل ادمین</h1>
+      <Formik
+        initialValues={{ name: '' }}
+        validate={values => {
+          let errors = {};
+          if (!values.username) {
+            errors.username = 'Required';
           }
-          
-          
-      }}
-    >
-      {props => (
-        <form onSubmit={props.handleSubmit}>
-   							<div className="field">
-								<input
-									type="text"
-									name="username"
-									className='w-100 p-1'
-									placeholder="پست الکترونیک"
-									onChange={props.handleChange}
-									onBlur={props.handleBlur}
-									value={props.values.email}
-								/>
-							</div>
-							<p className="error">
-								{props.errors.username && props.touched.username && props.errors.username}
-							</p>
-              <p>{errorlogin?.usernameErr}</p>
+          if (!values.password) {
+            errors.password = 'Required';
+          }
+          else if (values.password.length < 3) {
+            errors.password = "password is short"
+          }
+          return errors;
+        }}
+        onSubmit={(values) => {
+          if (values.username !== dataAdmin.username) {
+            setErorr({ usernameErr: "نام کاربری وارد شده اشتباه است" })
+          }
+          else if (values.password !== dataAdmin.password) {
+            setErorr({ passwordErr: "پسورد وارد شده اشتباه است" })
 
-							<div className="field">
-								<input
-									type="password"
-									name="password"
-									placeholder="کلمه عبور"
-									className='w-100 position-relative mt-4 p-1 mb-1'
-									onChange={props.handleChange}
-									onBlur={props.handleBlur}
-									value={props.values.password}
-								/>
-                <p className="error">
-								{props.errors.password && props.touched.password && props.errors.password}
-							</p>
+          }
+          else if (values.username == dataAdmin.username && values.password == dataAdmin.password) {
+            dispatch(login(true))
+            navigate('/paneladmin/orders', { replace })
+          }
+
+
+        }}
+      >
+        {props => (
+          <form onSubmit={props.handleSubmit}>
+            <div className="field">
+              <input
+                type="text"
+                name="username"
+                className='w-100 p-1'
+                placeholder="پست الکترونیک"
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.email}
+              />
+            </div>
+            <p className="error">
+              {props.errors.username && props.touched.username && props.errors.username}
+            </p>
+            <p>{errorlogin?.usernameErr}</p>
+
+            <div className="field">
+              <input
+                type="password"
+                name="password"
+                placeholder="کلمه عبور"
+                className='w-100 position-relative mt-4 p-1 mb-1'
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.password}
+              />
+              <p className="error">
+                {props.errors.password && props.touched.password && props.errors.password}
+              </p>
               <p>{errorlogin?.passwordErr}</p>
 
-                </div>
-          <button type="submit">ورود</button>
-          <Link to='/'>بازگشت به سایت</Link>
+            </div>
+            <button type="submit">ورود</button>
+            <Link to='/'>بازگشت به سایت</Link>
 
-        </form>
-      )}
-    </Formik>
-  </div>
-)}
+          </form>
+        )}
+      </Formik>
+    </div>
+  )
+}
 export default Login
