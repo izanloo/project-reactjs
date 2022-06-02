d8adbffc5221d466c2caf0e9d124a7b95016b30d
araks1397 <razieh.izanloo755@gmail.com>
2022-06-03 00:43:24 +0430

    feat:chek status payment use serchParam

--- src/pages/FinallBuy.js
+++ src/pages/FinallBuy.js
@@ -1,156 +1,254 @@
-import React, { useState } from 'react'
-import WithUser from '../Layouts/WithUser'
-import { Formik } from 'formik';
-import { Loginstyle } from '../Assest/Style/abstracts/Stylecomponent';
-import { Box } from '@mui/material';
-import { useSelector,useDispatch } from 'react-redux'
-import {useNavigate} from 'react-router-dom'
-// import persian from "react-date-object/calendars/persian"
-// import persian_fa from "react-date-object/locales/persian_fa"
+import React, { useState, useEffect } from "react";
+import WithUser from "../Layouts/WithUser";
+import { Grid, TextField, Button } from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import DatePicker from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import DateObject from "react-date-object";
+import { useSelector, useDispatch } from "react-redux";
 
-
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const validationSchema = yup.object().shape({
+  firstName: yup.string().required(" فیلد ضروری است"),
+  lastName: yup.string().required(" فیلد ضروری است"),
+  billingAddress: yup.string().required(" فیلد ضروری است"),
+  phone: yup.string().matches(phoneRegExp, "شماره همراه درست وارد نشده"),
+});
 function FinallBuy() {
+  // const { cartTotalAmount } = useSelector((state) => state.cart);
+  // const cart = useSelector((state) => state.cart.cartItems);
+  const [product,setproduct]=useState()
+  
+  // const [customer, setCustomer] = useState({});
+  const productarr=[]
+// const dispatch=useDispatch()
+// async function productfun(){
+//   cart.map((index)=>{
+//   productarr.push({"id":index.id,"quantity":index.cartQuantity })
+  
+// })
+   
+// }
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      billingAddress: "",
+      shippingAddress: "",
+      phone: "",
+      dateDeliver: "",
+    },
+    validationSchema,
+    onSubmit: async (values ,{resetForm}) => {
+      const customer ={
+        "orderStatus": "1",
+        firstName: values.firstName,
+        lastName: values.lastName,
+        phone: values.phone,
+        billingAddress: values.billingAddress,
+        "delivery":  values.dateDeliver,     
+      };
 
-// const date = new DateObject({ calendar: persian, locale: persian_fa })
 
-// console.log(date.format()) //۱۴۰۰/۰۴/۱۳
-// console.log(date.month.name) //تیر
-  const [newUser,setNewuser] =useState({})
-  const navigate = useNavigate()
+
+        localStorage.setItem('customer', JSON.stringify([customer]));
+      
+    
+        window.location.replace("http://127.0.0.1:5500/public/payment.html");
+
+    },
+  });
+  
+  // function toTimestamp(strDate){
+  //   var datum = Date.parse(strDate);
+  //   return datum;
+  //  } 
 
   return (
-    <Loginstyle>
-      <Formik
-        initialValues={{firstName: '',lastName:'', Address: '',phone:'',dateResive:'' }}
-        validate={values => {
-          let errors = {};
-          if (!values.firstName) {
-            errors.firstName = 'Required';
-          }
-          else{
-              setNewuser({ ...newUser, 'firstName':values.firstName })
-          }
-          if (!values.lastName) {
-            errors.lastName = 'Required';
-          }
-          else{
-            setNewuser({ ...newUser, 'lastName':values.lastName })
-        }
-          if (!values.Address) {
-            errors.Address = 'Required';
-          }
-          else{
-            setNewuser({ ...newUser, 'Address':values.Address })
-        }
-          if (!values.phone) {
-            errors.phone = 'Required';
-          }
-          else{
-            setNewuser({ ...newUser, 'phone':values.phone })
-        }
-          if (!values.dateResive) {
-            errors.dateResive = 'Required';
-          }
-          else{
-            setNewuser({ ...newUser, 'dateResive':values.dateResive })
-        }
-         console.log(newUser) 
-        }}
-        onSubmit={(values) => {
-          if (localStorage.getItem("customer") === null) {
-            localStorage.setItem('customer', JSON.stringify([newUser]));
-    
-          } else {
-            // localStorage.removeItem("customer")
-            // localStorage.setItem('customer', JSON.stringify([newUser]));
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container sx={{ m: 8 }}>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={2}>
+              <label> نام </label>
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                margin="dense"
+                size="small"
+                required
+                id="firstName"
+                name="firstName"
+                autoComplete="firstName"
+                color="success"
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+                helperText={
+                  formik.errors.firstName &&
+                  formik.touched.firstName &&
+                  formik.errors.firstName
+                }
+              />
+            </Grid>
+          </Grid>
 
-            const previousData = JSON.parse(localStorage.getItem("customer"));
-            localStorage.setItem('customer', JSON.stringify([...previousData,newUser]));
-          }
-          return navigate('/payment')
-        }}
-      >
-        {props => (
-          <Box sx={{ pt: '120px', maxWidth: { xs: '100%' } }}>
-            <form onSubmit={props.handleSubmit} >
-              <h1>نهایی کردن خرید</h1>
-              <div className="field">
-                <input
-                  type="text"
-                  name="firstName"
-                  className='w-100 p-1'
-                  placeholder="نام "
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.firstName}
-                />
-              </div>
-              <p className="error">
-                {props.errors.firstName && props.touched.firstName && props.errors.firstName}
-              </p>
-              <div className="field">
-                <input
-                  type="text"
-                  name="lastName"
-                  className='w-100 p-1'
-                  placeholder="نام خانوادگی "
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.lastName}
-                />
-              </div>
-              <p className="error">
-                {props.errors.lastName && props.touched.lastName && props.errors.lastName}
-              </p>
-              <div className="field">
-                <input
-                  name="Address"
-                  placeholder=" آدرس"
-                  className='w-100 position-relative mt-4 p-1 mb-1'
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.Address}
-                />
-              </div>
-                <p className="error">
-                  {props.errors.Address && props.touched.Address && props.errors.Address}
-                </p>
-                <div className="field">
-                <input
-                  name="phone"
-                  type="phone"
-                  placeholder=" تلفن همراه"
-                  className='w-100 position-relative mt-4 p-1 mb-1'
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.phone}
-                />
-              </div>
-                <p className="error">
-                  {props.errors.phone && props.touched.Address && props.errors.phone}
-                </p>
-                <div className="field">
-                <input
-                  name="dateResive"
-                  placeholder=" تاریخ تحویل"
-                  className='w-100 position-relative mt-4 p-1 mb-1'
-                  onChange={props.handleChange}
-                  onBlur={props.handleBlur}
-                  value={props.values.dateResive}
-                />
-                
-              </div>
-                <p className="error">
-                  {props.errors.dateResive && props.touched.dateResive && props.errors.dateResive}
-                </p>
-              <button type="submit">پرداخت</button>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={3}>
+              <label> نام خانوادگی </label>
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                margin="dense"
+                size="small"
+                required
+                id="lastName"
+                name="lastName"
+                placeholder=" نام خانوادگی  "
+                autoComplete="lastName"
+                color="success"
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+                helperText={
+                  formik.errors.lastName &&
+                  formik.touched.lastName &&
+                  formik.errors.lastName
+                }
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={2}>
+              <label>آدرس</label>
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                margin="dense"
+                size="small"
+                required
+                id="billingAddress"
+                name="billingAddress"
+                multiline
+                autoComplete="billingAddress"
+                color="success"
+                onChange={formik.handleChange}
+                value={formik.values.billingAddress}
+                helperText={
+                  formik.errors.billingAddress &&
+                  formik.touched.billingAddress &&
+                  formik.errors.billingAddress
+                }
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid>
+              <label> آدرس تحویل محصول</label>
+            </Grid>
+            <TextField
+              margin="dense"
+              size="small"
+              required
+              id="shippingAddress"
+              name="shippingAddress"
+              multiline
+              autoComplete="shippingAddress"
+              color="success"
+              onChange={formik.handleChange}
+              value={formik.values.shippingAddress}
+              helperText={
+                formik.errors.shippingAddress &&
+                formik.touched.shippingAddress &&
+                formik.errors.shippingAddress
+              }
+            />
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid>
+              <label>تلفن همراه</label>
+            </Grid>
 
-            </form>
+            <TextField
+              margin="dense"
+              size="small"
+              required
+              id="phone"
+              name="phone"
+              autoComplete="phone"
+              color="success"
+              onChange={formik.handleChange}
+              value={formik.values.phone}
+              helperText={
+                formik.errors.phone &&
+                formik.touched.phone &&
+                formik.errors.phone
+              }
+            />
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid>
+              <label>تاریخ تحویل </label>
+            </Grid>
 
-          </Box>
-        )}
-      </Formik>
-    </Loginstyle>
-  )
+            <DatePicker
+              calendar={persian}
+              locale={persian_fa}
+              calendarPosition="bottom-right"
+              weekPicker={false}
+              onChange={(e)=> formik.setFieldValue("dateDeliver", e.unix * 1000,true)}
+              value={formik.values.dateDeliver}
+              helperText={
+                formik.errors.dateDeliver &&
+                formik.touched.dateDeliver &&
+                formik.errors.dateDeliver
+              }
+              minDate={new DateObject({ calendar: persian })}
+              //disabled={sendToDefaultAddress}
+              // inputClass={Styles.custom_input}
+            />
+          </Grid>
+        </Grid>
+        <Button sx={{ mr: 80 }} variant="contained" type="submit">
+          پرداخت
+        </Button>
+      </form>
+  );
 }
-export default WithUser(FinallBuy)
+export default WithUser(FinallBuy);
--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,24 +1,26 @@
-import React, { useState } from 'react'
-import WithAdmin from '../Layouts/WithAdmin'
+import React, { useState,useEffect } from 'react'
+import WithUser from '../Layouts/WithUser'
 import Button from "@mui/material/Button";
 import axios from 'axios'
 import { Navigate } from 'react-router-dom'
 import { ArrowBack } from '@mui/icons-material';
 import { Link } from '@mui/material';
 import CancelPayment from './CancelPayment';
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 
 
 function Payment() {
   const [post, setPost] = useState(false)
   function successPayment() {
-    const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+    // const current = new Date();
+    // const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
     const LocalStorage = JSON.parse(localStorage.getItem("customer"));
     const LocalCart = JSON.parse(localStorage.getItem("cart"));
     let customerDetail = {
       'firstName': LocalStorage[0].firstName,
       'lastName': LocalStorage[0].lastName,
-      'Address': LocalStorage[0].Address,
+      'Address': LocalStorage[0].billingAddress,
       'phone': LocalStorage[0].phone,
 
       //تاریخ سفارش
@@ -38,25 +40,32 @@ function Payment() {
       data: {
         customerDetail,
         orderItems,
-        "orderDate": 1
+        "orderStatus": 1,
+        "delivery":LocalStorage[0].delivery
       },
 
     })
       .then(function (response) {
-        return <Navigate  to={successPayment} />
+        console.log(response)
       })
       .catch(function (error) {
         console.log(error);
       });
   
   }
+  const getStatus=()=>{
+    const statusPayment=searchParams.get("status")
+    if(statusPayment == 'success'){
+      successPayment()
+    }
+  }
+useEffect(()=>{getStatus()},[])
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  
   return (
     <>
-      <Button onClick={successPayment}>پرداخت</Button>
-     <Link to={CancelPayment}  >انصراف</Link>
+            {searchParams.get("status") == "success" ? "با موفقیت پرداخت شد" :"کنسل شد"}
     </>
   )
 }
-export default WithAdmin(Payment)
+export default WithUser(Payment)
